21a8580dbfa8241edb25427bd622ffa469f13969
Dermot Chiou <dermot@ifalo.com.tw>
2017-10-20 14:27:34 +0800

    update index link

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="row" v-for="app in apps" :key="app.app_id">
-            <a :href="rootUrl + '/download/#/' + app.app_id" class="btn btn-lg btn-primary col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-2 col-md-offset-5" >{{app.name}}</a>
+            <a :href="rootUrl + '/#/' + app.app_id" class="btn btn-lg btn-primary col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-2 col-md-offset-5" >{{app.name}}</a>
         </div>
     </div>
 </template>
@@ -17,8 +17,7 @@
               apps: ''
             }},
         computed: {
-            rootUrl: function()
-            {
+            rootUrl: function(){
                 return window.location.origin;
             }
         },
