494108dcdf51b0ff7fcdb8f6099b331982204bab
Dermot Chiou <dermot@ifalo.com.tw>
2017-11-29 14:48:54 +0800

    fix layout

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-        <div class="row" v-for="app in apps" :key="app.app_id">
-            <a :href="rootUrl + '/download/#/' + app.app_id" class="btn btn-lg btn-primary col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-2 col-md-offset-5" >{{app.name}}</a>
+        <div class="row">
+            <a v-for="(app, index) in apps" :href="rootUrl + '/download/#/' + app.app_id" class="btn btn-lg btn-primary col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-4" :class="[(index % 2 == 0)? 'col-md-offset-1' : 'col-md-offset-2']" >{{app.name}}</a>
         </div>
     </div>
 </template>
 <style>
     .row{
         margin: 5px 0;
+    }
+    .btn{
+        margin-bottom: 10px;
     }
 </style>
 <script>
